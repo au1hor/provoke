d390b32f18cf8071c380aad4cce033cb8c66100a
au1hor <authorestar@gmail.com>
2026-02-06 10:28:57 -0300

    Indicador de atack(Gigante slk)
    Indicador de ataque:
    --Atualmente um sprite de uma meia lua que rotaciona o jogador de acordo com a posição do mouse. bizarro né
    --Ele calcula a distancia de acordo com a magnitude tirado da diferença entre o player e o mouse adicionei um clamp pra ele ter limites min e max
    -- do kjrl

---
+++ Assets/scripts/AtackPoint.cs
@@ -0,0 +1,30 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AtackPoint : MonoBehaviour
+{
+    public GameObject player;
+    public float distance = 1.5f;
+    public float minDistance = 1.5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        Vector3 rawDir = mousePosition - player.transform.position;
+        float mouseDistance = rawDir.magnitude;
+        float finalDistance = math.clamp(mouseDistance,minDistance,distance);
+        Vector3 dir = rawDir.normalized;
+        transform.position = player.transform.position + dir * finalDistance;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,angle - 90f);
+    }
+}
