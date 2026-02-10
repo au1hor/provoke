cdd5c2bd1fb98172adceb57bd0a1c3828b69bd33
au1hor <authorestar@gmail.com>
2026-02-10 10:26:13 -0300

    Sound e faço a rotação da animação ser aleatoria ta meio bugada pq eu desenhei torto .... bizzarrro
    eba

--- Assets/scripts/player/PlayerAtack.cs
+++ Assets/scripts/player/PlayerAtack.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Collections;
+using System.Diagnostics;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.Rendering;
 
 public class PlayerAtack : MonoBehaviour
 {
     public GameObject slashSpr;
     public Sprite [] Sprites;
     public AtackPoint atackPoint;
+    public float speed = 0.05f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void instanceSprite()
@@ -17,22 +18,31 @@ public class PlayerAtack : MonoBehaviour
         StartCoroutine(SlashAnimation(Slash));
     }
     public void rotacion(GameObject obj)
-    {
+    {   UnityEngine.Random.Range(0,10);
         Vector3 dir = (atackPoint.transform.position - atackPoint.player.transform.position).normalized;
-        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(UnityEngine.Random.Range(-90,90),UnityEngine.Random.Range(-90,90)) * Mathf.Rad2Deg;
         obj.transform.rotation = Quaternion.Euler(0,0,angle - 90f);
+    }
+    public void soundEffect(GameObject obj)
+    {
+        AudioSource As = obj.GetComponent<AudioSource>();
+        As.pitch = UnityEngine.Random.Range(1f,2.1f);
+        As.Play();
+        Destroy(As,As.clip.length);
     }
     IEnumerator SlashAnimation(GameObject Slash)
     {
         SpriteRenderer sr = Slash.GetComponent<SpriteRenderer>();
+        rotacion(Slash);
+        soundEffect(Slash);
         for (int i = 0; i < Sprites.Length; i++)
         {
             sr.sprite = Sprites[i];
-            rotacion(Slash);
-             yield return new WaitForSeconds(0.1f);
+            
+             yield return new WaitForSeconds(speed);
         }
         
-        Destroy(Slash);
+        sr.enabled = false;
         
     }
     private void Update() {
