b7528a013ad0b4d3e43fefc90f746c52e99aa8dd
au1hor <authorestar@gmail.com>
2026-03-05 15:59:20 -0300

    Resolução do desktop Arrumada :>

--- Assets/scripts/DeskTopScrpts 1/AppsDesaktop.cs
+++ Assets/scripts/DeskTopScrpts 1/AppsDesaktop.cs
@@ -8,11 +8,13 @@ public class AppsDesaktop : MonoBehaviour, IPointerEnterHandler,IPointerExitHand
     public void OnPointerEnter(PointerEventData data)
     {
         this.gameObject.transform.localScale = new Vector2(1.2f,1.2f);
+        this.gameObject.GetComponentInChildren<TMP_Text>().color = Color.green;
          showCurrencyArea.text = this.gameObject.name;
     }
     public void OnPointerExit(PointerEventData data)
     {
         this.gameObject.transform.localScale = Vector2.one;
+         this.gameObject.GetComponentInChildren<TMP_Text>().color = Color.white;
          showCurrencyArea.text = "Desktop  - Area Rest";
     }
 }
